30a6173ce6074b7e1bca539f03c6e9c6b770df42
Lam Pham <alldaylam3@gmail.com>
2021-06-10 22:59:54 -0400

    rand-img fixes

--- scripts/rand-img.js
+++ scripts/rand-img.js
@@ -4,8 +4,8 @@ $(window).on("load", function() {
     set_image();
 });
 
-var imglist = ["assets/allpjs-images/rand/1.png", "assets/allpjs-images/rand/2.jpg", "assets/allpjs-images/rand/3.jpg", "assets/allpjs-images/rand/4.png", "assets/allpjs-images/rand/5.jpeg", "assets/allpjs-images/rand/6.jpeg", "assets/allpjs-images/rand/7.jpg", "assets/allpjs-images/rand/8.jpg", "assets/allpjs-images/rand/9.jpg"];
-var imgpath = "assets/allpjs-images/rand/1.png";
+var imglist = ["assets/allpjs-images/rand/1.jpg", "assets/allpjs-images/rand/2.jpg", "assets/allpjs-images/rand/3.jpg", "assets/allpjs-images/rand/4.jpg", "assets/allpjs-images/rand/5.jpg", "assets/allpjs-images/rand/6.jpg", "assets/allpjs-images/rand/7.jpg", "assets/allpjs-images/rand/8.jpg", "assets/allpjs-images/rand/9.jpg"];
+var imgpath = "assets/allpjs-images/rand/1.jpg";
 
 function set_image() {
     randomNumber = Math.floor(Math.random() * imglist.length);
