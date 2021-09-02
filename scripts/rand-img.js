1c6d3b9aee8f9d51c32834a1cbdf8269754215b6
Lam Pham <alldaylam3@gmail.com>
2021-09-01 23:40:19 -0400

    rand-img

--- scripts/rand-img.js
+++ scripts/rand-img.js
@@ -4,7 +4,7 @@ $(window).on("load", function() {
     set_image();
 });
 
-var imglist = ["assets/allpjs-images/rand/1.png", "assets/allpjs-images/rand/2.JPG", "assets/allpjs-images/rand/3.JPEG", "assets/allpjs-images/rand/4.JPG", "assets/allpjs-images/rand/5.png", "assets/allpjs-images/rand/6.png", "assets/allpjs-images/rand/7.png", "assets/allpjs-images/rand/8.JPG", "assets/allpjs-images/rand/9.png", "assets/allpjs-images/rand/10.JPG"];
+var imglist = ["assets/allpjs-images/rand/1.png", "assets/allpjs-images/rand/2.png", "assets/allpjs-images/rand/3.JPEG", "assets/allpjs-images/rand/4.png", "assets/allpjs-images/rand/5.png", "assets/allpjs-images/rand/6.png", "assets/allpjs-images/rand/7.png", "assets/allpjs-images/rand/8.png", "assets/allpjs-images/rand/9.png", "assets/allpjs-images/rand/10.JPG"];
 var imgpath = "assets/allpjs-images/rand/1.png";
 
 function set_image() {
