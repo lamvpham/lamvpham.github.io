6c101bc0309818790deb9843fe463c0d26645f8d
Lam Pham <alldaylam3@gmail.com>
2021-10-01 19:06:39 -0400

    opacity change

--- scripts/title.js
+++ scripts/title.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $(window).scroll(function() {
         if ($(this).scrollTop() > 24) {
-            $(".title-text").css({ "opacity": "0.03" })
+            $(".title-text").css({ "opacity": "0.05" })
         } else {
             $(".title-text").css({ "opacity": "1" })
         }
