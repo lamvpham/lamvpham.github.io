5cf2d83bdbaae1ac1a49c5e72e5b0511de5ce52e
Lam Pham <alldaylam3@gmail.com>
2023-10-19 21:01:33 -0400

    new case study added

--- scripts/project-hovers.js
+++ scripts/project-hovers.js
@@ -1,6 +1,7 @@
 // main project hover components
 var textHover1 = document.querySelector('#project-text-1');
 var textHover2 = document.querySelector('#project-text-2');
+var textHover3 = document.querySelector('#project-text-3');
 var imageHover = document.querySelector('#project-image');
 var projectHover = document.getElementById('pj-hovers');
 
@@ -11,10 +12,13 @@ var bottombar = document.querySelector('.bot');
 var bodycontent = document.getElementById('copy');
 var pjtitle1 = document.getElementById('proj-text-1');
 var pjtitle2 = document.getElementById('proj-text-2');
+var pjtitle3 = document.getElementById('proj-text-3');
 var stars1 = document.getElementById('stars-1');
 var stars2 = document.getElementById('stars-2');
 var stars3 = document.getElementById('stars-3');
 var stars4 = document.getElementById('stars-4');
+var stars5 = document.getElementById('stars-5');
+var stars6 = document.getElementById('stars-6');
 
 // pjtitle1.style.opacity = "0";
 // pjtitle2.style.opacity = "0";
@@ -24,14 +28,18 @@ textHover1.addEventListener('mouseover', showImage1);
 textHover1.addEventListener('mouseout', hideImage1);
 textHover2.addEventListener('mouseover', showImage2);
 textHover2.addEventListener('mouseout', hideImage2);
+textHover3.addEventListener('mouseover', showImage3);
+textHover3.addEventListener('mouseout', hideImage3);
 
 function showImage1() {
     projectHover.classList.add("proj-img-1")
     projectHover.classList.remove("proj-img-2")
+    projectHover.classList.remove("proj-img-3")
     imageHover.style.opacity = '0.75';
+    bodycontent.style.opacity = '0.18';
     pjtitle1.style.opacity = "1";
     pjtitle2.style.opacity = "0";
-    bodycontent.style.opacity = '0.18';
+    pjtitle3.style.opacity = "0";
     header.style.opacity = '0';
     topbar.style.opacity = '0.18';
     bottombar.style.opacity = '0.18';
@@ -55,10 +63,12 @@ function hideImage1() {
 function showImage2() {
     projectHover.classList.add("proj-img-2")
     projectHover.classList.remove("proj-img-1")
+    projectHover.classList.remove("proj-img-3")
     imageHover.style.opacity = '0.75';
+    bodycontent.style.opacity = '0.18';
     pjtitle2.style.opacity = "1";
     pjtitle1.style.opacity = "0";
-    bodycontent.style.opacity = '0.18';
+    pjtitle3.style.opacity = "0";
     header.style.opacity = '0';
     topbar.style.opacity = '0.18';
     bottombar.style.opacity = '0.18';
@@ -76,5 +86,34 @@ function hideImage2() {
     pjtitle2.style.opacity = "0";
     stars3.style.color = '#8a8dea';
     stars4.style.color = '#8a8dea';
+    textHover1.style.opacity = '1';
+}
+
+function showImage3() {
+    projectHover.classList.add("proj-img-3")
+    projectHover.classList.remove("proj-img-1")
+    projectHover.classList.remove("proj-img-2")
+    imageHover.style.opacity = '0.75';
+    bodycontent.style.opacity = '0.18';
+    pjtitle3.style.opacity = "1";
+    pjtitle2.style.opacity = "0";
+    pjtitle1.style.opacity = "0";
+    header.style.opacity = '0';
+    topbar.style.opacity = '0.18';
+    bottombar.style.opacity = '0.18';
+    stars5.style.color = '#202020';
+    stars6.style.color = '#202020';
+    textHover1.style.opacity = '0.85';
+}
+
+function hideImage3() {
+    imageHover.style.opacity = '0';
+    header.style.opacity = '0.1';
+    topbar.style.opacity = '1';
+    bottombar.style.opacity = '1';
+    bodycontent.style.opacity = '1';
+    pjtitle3.style.opacity = "0";
+    stars5.style.color = '#8a8dea';
+    stars6.style.color = '#8a8dea';
     textHover1.style.opacity = '1';
 }
