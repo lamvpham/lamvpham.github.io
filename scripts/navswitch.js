5815f981c4fc0a34e7091ca35128fd0e51891906
Lam Pham <alldaylam3@gmail.com>
2023-12-26 15:42:34 -0500

    prose, shortened paragraphs, rotating stars, highlights, brighter text&hover,

--- scripts/navswitch.js
+++ scripts/navswitch.js
@@ -1,22 +1,66 @@
-
 var workNav = document.getElementById('work');
 var workContent = document.getElementById('workcontent');
 var aboutNav = document.getElementById('about');
 var aboutContent = document.getElementById('aboutcontent');
+var proseNav = document.getElementById('prose');
+var proseContent = document.getElementById('prosecontent');
+
 
 if (window.location.hash === '#about') {
     showAbout();
-} else {
+} 
+
+else if (window.location.hash === '#prose') {
+    showProse();
+} 
+
+else {
     aboutContent.style.display = 'none';
+    proseContent.style.display = 'none';
 }
+
 
 workNav.addEventListener("click", showWork);
 aboutNav.addEventListener("click", showAbout);
+proseNav.addEventListener("click", showProse);
 
+
+function showProse() {
+    workContent.style.display = 'none';
+    aboutContent.style.display = 'none';
+    proseContent.style.display = 'block';
+
+    if (window.location.hash === '#about') {
+        history.replaceState({}, document.title, window.location.pathname);
+    }
+
+    proseNav.classList.add("italics");
+    proseNav.classList.add("underline");
+    proseNav.classList.remove("highlight");
+    proseNav.classList.remove("texthover");
+    proseNav.classList.add("glowing");
+
+    aboutNav.classList.add("highlight");
+    aboutNav.classList.add("texthover");
+    aboutNav.classList.remove("italics");
+    aboutNav.classList.remove("underline");
+    aboutNav.classList.remove("glowing");
+
+    workNav.classList.add("highlight");
+    workNav.classList.add("texthover");
+    workNav.classList.remove("italics");
+    workNav.classList.remove("underline");
+    workNav.classList.remove("glowing");
+}
 
 function showAbout() {
     workContent.style.display = 'none';
     aboutContent.style.display = 'block';
+    proseContent.style.display = 'none';
+
+    if (window.location.hash === '#prose') {
+        history.replaceState({}, document.title, window.location.pathname);
+    }
 
     aboutNav.classList.add("italics");
     aboutNav.classList.add("underline");
@@ -29,13 +73,24 @@ function showAbout() {
     workNav.classList.remove("italics");
     workNav.classList.remove("underline");
     workNav.classList.remove("glowing");
+
+    proseNav.classList.add("highlight");
+    proseNav.classList.add("texthover");
+    proseNav.classList.remove("italics");
+    proseNav.classList.remove("underline");
+    proseNav.classList.remove("glowing");
 }
 
 function showWork() {
     workContent.style.display = 'block';
     aboutContent.style.display = 'none';
+    proseContent.style.display = 'none'
 
     if (window.location.hash === '#about') {
+        history.replaceState({}, document.title, window.location.pathname);
+    }
+
+    else if (window.location.hash === '#prose') {
         history.replaceState({}, document.title, window.location.pathname);
     }
 
@@ -50,4 +105,10 @@ function showWork() {
     aboutNav.classList.remove("italics");
     aboutNav.classList.remove("underline");
     aboutNav.classList.remove("glowing");
+
+    proseNav.classList.add("highlight");
+    proseNav.classList.add("texthover");
+    proseNav.classList.remove("italics");
+    proseNav.classList.remove("underline");
+    proseNav.classList.remove("glowing");
 }
