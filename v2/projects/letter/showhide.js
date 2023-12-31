f7b5376462d98a34d1f45359559eeda4d626c15c
Lam Pham <alldaylam3@gmail.com>
2023-12-31 15:43:31 -0500

    letter 2023

--- v2/projects/letter/showhide.js
+++ v2/projects/letter/showhide.js
@@ -1,5 +1,5 @@
 function showContent(year) {
-    var arrayOfYears = ["2020", "2021", "2022"];
+    var arrayOfYears = ["2020", "2021", "2022", "2023"];
     let showhide;
   
     // hide all years' content
