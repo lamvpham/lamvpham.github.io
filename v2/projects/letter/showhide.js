7f260037214c659bbf4efe38bd00e946cd4d1ccd
Lam <lamvpham3@gmail.com>
2025-12-29 23:16:28 -0500

    2025 eoy letter

--- v2/projects/letter/showhide.js
+++ v2/projects/letter/showhide.js
@@ -1,5 +1,5 @@
 function showContent(year) {
-    var arrayOfYears = ["2020", "2021", "2022", "2023", "2024"];
+    var arrayOfYears = ["2020", "2021", "2022", "2023", "2024", "2025"];
     let showhide;
   
     // hide all years' content
