394b975f90e6cf86d94c4d7cc12b9c6c775f4198
Lam <lamvpham3@gmail.com>
2026-04-12 14:49:29 -0700

    cleaned up portfolio

--- scripts/navswitch.js
+++ scripts/navswitch.js
@@ -1,15 +1,15 @@
 var workNav = document.getElementById('work');
 var workContent = document.getElementById('workcontent');
-var aboutNav = document.getElementById('about');
-var aboutContent = document.getElementById('aboutcontent');
-var proseNav = document.getElementById('prose');
-var proseContent = document.getElementById('prosecontent');
+// var aboutNav = document.getElementById('about');
+// var aboutContent = document.getElementById('aboutcontent');
+var archiveNav = document.getElementById('archive');
+var archiveContent = document.getElementById('archivecontent');
 
 // script for url hash changes
 function hideAllContent() {
     workContent.style.display = 'none';
-    aboutContent.style.display = 'none';
-    proseContent.style.display = 'none';
+    // aboutContent.style.display = 'none';
+    archiveContent.style.display = 'none';
 }
 
 function handleHashChange() {
@@ -17,10 +17,11 @@ function handleHashChange() {
 
     hideAllContent();
 
-    if (hash === '#about') {
-        showAbout();
-    } else if (hash === '#prose') {
-        showProse();
+    // if (hash === '#about') {
+    //     showAbout();
+    // } else
+    if (hash === '#archive') {
+        showArchive();
     } else {
         showWork();
     }
@@ -32,30 +33,30 @@ handleHashChange();
 
 // showing and hiding content
 workNav.addEventListener("click", showWork);
-aboutNav.addEventListener("click", showAbout);
-proseNav.addEventListener("click", showProse);
+// aboutNav.addEventListener("click", showAbout);
+archiveNav.addEventListener("click", showArchive);
 
 
-function showProse() {
+function showArchive() {
     workContent.style.display = 'none';
-    aboutContent.style.display = 'none';
-    proseContent.style.display = 'block';
+    // aboutContent.style.display = 'none';
+    archiveContent.style.display = 'block';
 
-    if (window.location.hash === '#about') {
-        history.pushState({}, document.title, window.location.pathname);
-    }
+    // if (window.location.hash === '#about') {
+    //     history.pushState({}, document.title, window.location.pathname);
+    // }
 
-    proseNav.classList.add("italics");
-    proseNav.classList.add("underline");
-    proseNav.classList.remove("highlight");
-    proseNav.classList.remove("texthover");
-    proseNav.classList.add("glowing");
+    archiveNav.classList.add("italics");
+    archiveNav.classList.add("underline");
+    archiveNav.classList.remove("highlight");
+    archiveNav.classList.remove("texthover");
+    archiveNav.classList.add("glowing");
 
-    aboutNav.classList.add("highlight");
-    aboutNav.classList.add("texthover");
-    aboutNav.classList.remove("italics");
-    aboutNav.classList.remove("underline");
-    aboutNav.classList.remove("glowing");
+    // aboutNav.classList.add("highlight");
+    // aboutNav.classList.add("texthover");
+    // aboutNav.classList.remove("italics");
+    // aboutNav.classList.remove("underline");
+    // aboutNav.classList.remove("glowing");
 
     workNav.classList.add("highlight");
     workNav.classList.add("texthover");
@@ -64,44 +65,44 @@ function showProse() {
     workNav.classList.remove("glowing");
 }
 
-function showAbout() {
-    workContent.style.display = 'none';
-    aboutContent.style.display = 'block';
-    proseContent.style.display = 'none';
-
-    if (window.location.hash === '#prose') {
-        history.pushState({}, document.title, window.location.pathname);
-    }
-
-    aboutNav.classList.add("italics");
-    aboutNav.classList.add("underline");
-    aboutNav.classList.remove("highlight");
-    aboutNav.classList.remove("texthover");
-    aboutNav.classList.add("glowing");
-
-    workNav.classList.add("highlight");
-    workNav.classList.add("texthover");
-    workNav.classList.remove("italics");
-    workNav.classList.remove("underline");
-    workNav.classList.remove("glowing");
-
-    proseNav.classList.add("highlight");
-    proseNav.classList.add("texthover");
-    proseNav.classList.remove("italics");
-    proseNav.classList.remove("underline");
-    proseNav.classList.remove("glowing");
-}
+// function showAbout() {
+//     workContent.style.display = 'none';
+//     aboutContent.style.display = 'block';
+//     archiveContent.style.display = 'none';
+//
+//     if (window.location.hash === '#archive') {
+//         history.pushState({}, document.title, window.location.pathname);
+//     }
+//
+//     aboutNav.classList.add("italics");
+//     aboutNav.classList.add("underline");
+//     aboutNav.classList.remove("highlight");
+//     aboutNav.classList.remove("texthover");
+//     aboutNav.classList.add("glowing");
+//
+//     workNav.classList.add("highlight");
+//     workNav.classList.add("texthover");
+//     workNav.classList.remove("italics");
+//     workNav.classList.remove("underline");
+//     workNav.classList.remove("glowing");
+//
+//     archiveNav.classList.add("highlight");
+//     archiveNav.classList.add("texthover");
+//     archiveNav.classList.remove("italics");
+//     archiveNav.classList.remove("underline");
+//     archiveNav.classList.remove("glowing");
+// }
 
 function showWork() {
     workContent.style.display = 'block';
-    aboutContent.style.display = 'none';
-    proseContent.style.display = 'none'
+    // aboutContent.style.display = 'none';
+    archiveContent.style.display = 'none'
 
-    if (window.location.hash === '#about') {
-        history.pushState({}, document.title, window.location.pathname);
-    }
+    // if (window.location.hash === '#about') {
+    //     history.pushState({}, document.title, window.location.pathname);
+    // }
 
-    else if (window.location.hash === '#prose') {
+    if (window.location.hash === '#archive') {
         history.pushState({}, document.title, window.location.pathname);
     }
 
@@ -111,15 +112,15 @@ function showWork() {
     workNav.classList.remove("texthover");
     workNav.classList.add("glowing");
 
-    aboutNav.classList.add("highlight");
-    aboutNav.classList.add("texthover");
-    aboutNav.classList.remove("italics");
-    aboutNav.classList.remove("underline");
-    aboutNav.classList.remove("glowing");
+    // aboutNav.classList.add("highlight");
+    // aboutNav.classList.add("texthover");
+    // aboutNav.classList.remove("italics");
+    // aboutNav.classList.remove("underline");
+    // aboutNav.classList.remove("glowing");
 
-    proseNav.classList.add("highlight");
-    proseNav.classList.add("texthover");
-    proseNav.classList.remove("italics");
-    proseNav.classList.remove("underline");
-    proseNav.classList.remove("glowing");
+    archiveNav.classList.add("highlight");
+    archiveNav.classList.add("texthover");
+    archiveNav.classList.remove("italics");
+    archiveNav.classList.remove("underline");
+    archiveNav.classList.remove("glowing");
 }
